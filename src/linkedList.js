3f411fd0e2ee94c43da3309a707556991a56e0d8
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-05-01 11:56:38 -0400

    feat: Add append function and tests

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,10 +1,28 @@
 export class LinkedList {
-    
+    head = null;
+    tail = null;
+    length = 0;
+
+    append(value) {
+        let node = new Node(value, null);
+
+        if(this.tail !== null) this.tail.next = node;
+        this.tail = node;
+
+        if(this.length === 0) this.head = node;
+
+        this.length += 1;
+    }
+
+    prepend(value) {
+        
+    }
 }
 
 export class Node {
     constructor(value, next) {
         this.next = next;
+        this.value = value;
     }
 
     value = null;
--- src/test/linkedList.test.js
+++ src/test/linkedList.test.js
@@ -1,9 +1,45 @@
 import { LinkedList } from "../linkedList.js";
 import { Node } from "../linkedList.js";
 
+function printList(linkedList) {
+    let node = linkedList.head;
+
+    if(node === null) console.log("Nothing to print");
+
+    while(node !== null) {
+        console.log(node.value);
+        node = node.next;
+    }
+}
+
 describe('Linked List functionality', () => {
+    let list = undefined;
+
+    beforeAll(() => {
+        list = new LinkedList();
+
+        list.append("dog");
+        list.append("cat");
+        list.append("parrot");
+        list.append("hamster");
+        list.append("snake");
+        list.append("turtle");
+    });
+    
     it('Exists', () => {
         expect(LinkedList).toBeDefined();
+    });
+
+    it('Appends a value', () => {
+        list.append("Hello");
+
+        expect(list.tail.value).toEqual("Hello");
+    });
+
+    it('Prepends a value', () => {
+        list.prepend("Hey");
+
+        expect(list.head.value).toEqual("Hey");
     });
 });
 
